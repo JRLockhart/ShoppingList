1fefa9bb86ca553b1f0495bc35ead292ee726acb
James Lockhart <james.lockhart117@gmail.com>
2018-03-28 10:04:04 +0300

    Added a save function

--- shopping_list/shopping_list/shopping_list.py
+++ shopping_list/shopping_list/shopping_list.py
@@ -25,23 +25,39 @@ def add_to_list(new_item):
     shopping_list.append(new_item)
     print ("Added {}. List now has {} items".format(new_item, len(shopping_list)))
 
+def main():
+
+    while True:
+        # ask for new items
+        new_item = input("> ")
+
+        # be able to quit the app
+        if new_item == 'DONE':
+            break
+        elif new_item == 'HELP':
+            show_help()
+            continue
+        elif new_item == 'SHOW':
+            show_list()
+            continue
+        add_to_list(new_item)
+    #show list to user when when loop is exited from
+    show_list()
+    
+#adding a save function    
+def open_save():
+  print("Enter the name of your file")
+  file = input('Enter your name: ')
+
+  try:
+       with open(file, 'w') as file:
+         for items in shopping_list:
+            file.write(str(items) + "\n")
+  except:
+    print("try again")    
+
+open_save()
 show_help()
-
-while True:
-    # ask for new items
-    new_item = input("> ")
-
-    # be able to quit the app
-    if new_item == 'DONE':
-        break
-    elif new_item == 'HELP':
-        show_help()
-        continue
-    elif new_item == 'SHOW':
-        show_list()
-        continue
-    add_to_list(new_item)
+main()
 
 
-#show list to user when when loop is exited from
-show_list()
