b0eb3cdddbec2dd001d57820e797059ac057e23d
James Lockhart <james.lockhart117@gmail.com>
2018-03-28 09:16:56 +0300

    Added more functionality 'HELP and SHOW' commands

--- shopping_list/shopping_list/shopping_list.py
+++ shopping_list/shopping_list/shopping_list.py
@@ -1,26 +1,47 @@
+
+
 # make a list to hold onto our items
 shopping_list = []
-
-# print out instruction on how to use the app
 print("What should we pick up at the store?")
-print("Enter 'DONE' to stop adding items")
+
+
+def show_help():
+    # print out instruction on how to use the app    
+    print("""
+Enter 'DONE' to stop adding items.
+Enter 'HELP' to stop adding items.
+Enter 'SHOW' to stop adding items.
+""")
+
+def show_list():
+    # print out the list
+    print("Here's your list:")
+
+    for item in shopping_list:
+        print(item)
+
+def add_to_list(new_item):
+    # add new items to our list
+    shopping_list.append(new_item)
+    print ("Added {}. List now has {} items".format(new_item, len(shopping_list)))
+
+show_help()
 
 while True:
-
     # ask for new items
     new_item = input("> ")
 
     # be able to quit the app
     if new_item == 'DONE':
         break
+    elif new_item == 'HELP':
+        show_help()
+        continue
+    elif new_item == 'SHOW':
+        show_list()
+        continue
+    add_to_list(new_item)
 
-    # add new items to our list
-    shopping_list.append(new_item)
 
-
-
-# print out the list
-print("Here's your list:")
-
-for item in shopping_list:
-    print(item)
+#show list to user when when loop is exited from
+show_list()
